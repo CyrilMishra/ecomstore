a8697ca37329fd135d12fd8c4ff88c58fde41d83
CyrilMishra <mishracyril@gmail.com>
2025-01-21 11:45:52 +0530

    Initial project name setup

--- ecommerce_store/ecommerce_store/settings.py
+++ ecommerce_store/ecommerce_store/settings.py
@@ -37,6 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'reviews',
+    'products',
+    'orders',
+    'notifications',
+    'cart',
+    'cache',
+    'analytics',
+    'admin_dashboard',
 ]
 
 MIDDLEWARE = [
